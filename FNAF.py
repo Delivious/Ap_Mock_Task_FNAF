968ea995f467a294f541b487cb9e6257b2f20983
Bobusslob <bader.ori@gmail.com>
2025-12-08 12:26:40 -0500

    all the new freddys

--- FNAF.py
+++ FNAF.py
@@ -5,6 +5,12 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("My Pygame Window")
+freddy1=pygame.image.load("freddy1.png")
+freddy2=pygame.image.load("freddy2.png")
+freddy3=pygame.image.load("freddy3.png")
+freddy4=pygame.image.load("freddy4.png")
+freddy5=pygame.image.load("freddy5.png")
+freddy6=pygame.image.load("freddy6.png")
 running = True
 while running:
     # Event handling
@@ -16,7 +22,7 @@ while running:
 
     # Drawing
     screen.fill((0, 0, 0)) # Fill the screen with black (RGB)
-
+    screen.blit(freddy6, (0,0))
     # Update the display
     pygame.display.flip() # or pygame.display.update()
 #^ Use of AI for pygame starter ^
