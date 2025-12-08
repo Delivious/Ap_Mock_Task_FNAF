68c17247bf85d072c926fe64afe5d1999855d48f
Bobusslob <bader.ori@gmail.com>
2025-12-08 13:32:59 -0500

    fixys

--- FNAF.py
+++ FNAF.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import time
 pygame.init()
     
 screen_width = 800
@@ -9,8 +11,7 @@ freddy1=pygame.image.load("freddy1.png")
 freddy2=pygame.image.load("freddy2.png")
 freddy3=pygame.image.load("freddy3.png")
 freddy4=pygame.image.load("freddy4.png")
-freddy5=pygame.image.load("freddy5.png")
-freddy6=pygame.image.load("freddy6.png")
+
 running = True
 while running:
     # Event handling
@@ -21,8 +22,19 @@ while running:
     # Game logic (e.g., update object positions)
 
     # Drawing
-    screen.fill((0, 0, 0)) # Fill the screen with black (RGB)
-    screen.blit(freddy6, (0,0))
+    screen.blit(freddy1, (0,0))
+    time.sleep(.2)
+    pygame.display.update()
+    screen.blit(freddy2, (0,0))
+    time.sleep(.4)
+    pygame.display.update()
+    screen.blit(freddy3, (0,0))
+    time.sleep(.2)
+    pygame.display.update()
+    screen.blit(freddy4, (0,0))
+    time.sleep(.1)
+    pygame.display.update()
+    time.sleep(.2)
     # Update the display
     pygame.display.flip() # or pygame.display.update()
 #^ Use of AI for pygame starter ^
