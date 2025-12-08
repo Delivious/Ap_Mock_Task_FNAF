f9adfe503648a21ffc4e61543c5d75845b85179e
Delivious <mason31210@gmail.com>
2025-12-08 14:12:05 -0500

    pretty much finished class

--- night1.py
+++ night1.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import threading
 import random
-
+# https://www.youtube.com/watch?v=MwbXp6_C5i8 is the link to the video for threading and running multiple functions concurrently
 pygame.init()
 class Anim: #Masons class and funcs
     def __init__(self, animatronic, difficulty, curRoom, rooms):
@@ -36,14 +36,94 @@ class Anim: #Masons class and funcs
                 moveArea(move)
             else:
                 move=False
-                moveArea(move)
-    def moveArea(move):
-
-
+                placeholder=moveArea(move)
+    def moveArea(self, move):
+        if move == True:
+            if self._animatronic == "Freddy":
+                if self._curRoom.lower()=="showstage":
+                    toMove=["Dining", "Restrooms"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="dining":
+                    toMove=["EastHall", "Restrooms", "Kitchen", "Dining"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="kitchen":
+                    toMove=["Dining", "EastHall", "Kitchen"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="restroom":
+                    toMove=["Dining", "Kitchen", "Restroom"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="easthall":
+                    toMove=["Dining", "EastHallCorner","EastHall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="easthallcorner":
+                    toMove=["Office", "EastHallCorner","EastHall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+            elif self._animatronic == "Chica":
+                if self._curRoom.lower()=="showstage":
+                    toMove=["Dining", "Restrooms"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="dining":
+                    toMove=["EastHall", "Restrooms", "Kitchen", "Dining"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="kitchen":
+                    toMove=["Dining", "EastHall", "Kitchen"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="restroom":
+                    toMove=["Dining", "Kitchen", "Restroom"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="easthall":
+                    toMove=["Dining", "EastHallCorner","EastHall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="easthallcorner":
+                    toMove=["Office", "EastHallCorner","EastHall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+            elif self._animatronic == "Bonnie":
+                if self._curRoom.lower()=="showstage":
+                    toMove=["Dining", "Backstage"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="dining":
+                    toMove=["WestHall", "BackStage", "Dining"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="backstage":
+                    toMove=["Dining", "WestHall", "Dining"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="westhall":
+                    toMove=["Dining", "WestHallCorner", "SupplyCloset"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="westhallcorner":
+                    toMove=["Office", "EastHallCorner","westhall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                elif self._curRoom.lower()=="supplycloset":
+                    toMove=["Office","EastHall"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+            elif self._animatronic == "Foxy":
+                if self._curRoom.lower()=="piratecove":
+                    toMove=["PirateCove"]
+                    whereMove=random.choice(toMove)
+                    self._curRoom=whereMove
+                
 freddy=Anim("Freddy", 3, "ShowStage", ["ShowStage", "Dining", "Restrooms", "Kitchen", "EastHall","EastHallCorner"])
 chica=Anim("Chica", 3, "ShowStage", ["ShowStage", "Dining", "Restrooms", "Kitchen", "EastHall","EastHallCorner"])
 bonnie=Anim("Bonnie", 3, "ShowStage", ["ShowStage", "Dining", "Backstage", "SupplyCloset", "WestHall","WestHallCorner"])
-foxy=Anim("Focxy", 3, "PiratesCove", ["PiratesCove", "Dining", "WestHall","WestHallCorner"])
+foxy=Anim("Foxy", 3, "PiratesCove", ["PiratesCove", "Dining", "WestHall","WestHallCorner"])
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
