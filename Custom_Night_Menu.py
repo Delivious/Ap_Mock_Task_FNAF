36b1f69b49f57f560a09a5bed6c36e17ca2d8475
Bobusslob <bader.ori@gmail.com>
2025-12-11 08:24:16 -0500

    the ready button

--- Custom_Night_Menu.py
+++ Custom_Night_Menu.py
@@ -8,13 +8,15 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (68, 67, 68)
 font = pygame.font.SysFont('Arial', 72)
+font2 = pygame.font.SysFont('Arial', 62)
 freddy = pygame.image.load("fredder.png")
 bonnie = pygame.image.load("bonnie.png")
 chica = pygame.image.load("chica.png")
 foxy = pygame.image.load("froxy.png")
-
+diflevels=font2.render('(0-2)easy, (3-6)med, (7-12)hard, (12+)extreme', True, WHITE)
+ready= pygame.Rect((1100,950),(230,150))
+ready_text = font.render('Ready', True, WHITE)
 fredname = font.render('Freddy', True, WHITE)
-
 
 
 fredAdd = pygame.Rect((400,560),(75,100))
@@ -44,6 +46,7 @@ def difficuties():
 
     # draw images and buttons
     screen2.blit(freddy, (400, 200))
+    screen2.blit(fredname, (460, 100))
     pygame.draw.rect(screen2, GREY, fredAdd)
     pygame.draw.rect(screen2, GREY, fredminus)
     pygame.draw.lines(screen2, WHITE, False, [[420.5,610],[450,640],[420.5,610],[450,580]], 5)
@@ -60,6 +63,7 @@ def difficuties():
 
 
     screen2.blit(chica, (850, 200))
+    screen2.blit(chicaname, (920, 100))
     pygame.draw.rect(screen2, GREY, chicaAdd)
     pygame.draw.rect(screen2, GREY, chicaminus)
     pygame.draw.lines(screen2, WHITE, False, [[872.5,610],[902,640],[872.5,610],[902,580]], 5)
@@ -72,6 +76,7 @@ def difficuties():
     screen2.blit(font.render(str(chicadif), True, WHITE), (1000, 580))
 
     screen2.blit(bonnie, (1300, 200))
+    screen2.blit(bonniename, (1370, 100))
     pygame.draw.rect(screen2, GREY, bonnieAdd)
     pygame.draw.rect(screen2, GREY, bonnieminus)
     pygame.draw.lines(screen2, WHITE, False, [[1317.5,610],[1347,640],[1317.5,610],[1347,580]], 5)
@@ -84,6 +89,7 @@ def difficuties():
     screen2.blit(font.render(str(bonniedif), True, WHITE), (1450, 580))
 
     screen2.blit(foxy, (1750, 200))
+    screen2.blit(foxyname, (1820, 100))
     pygame.draw.rect(screen2, GREY, foxyAdd)
     pygame.draw.rect(screen2, GREY, foxyminus)
     pygame.draw.lines(screen2, WHITE, False, [[1767.5,610],[1797,640],[1767.5,610],[1797,580]], 5)
@@ -94,6 +100,10 @@ def difficuties():
 
     # draw number
     screen2.blit(font.render(str(foxydif), True, WHITE), (1900, 580))
+
+    screen2.blit(diflevels, (600, 825))
+    pygame.draw.rect(screen2, GREY, ready)
+    screen2.blit(ready_text, (1115, 985))
 
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:
@@ -127,6 +137,10 @@ def difficuties():
             elif foxyminus.collidepoint(mouse_pos):
                 if foxydif > 0:
                     foxydif -= 1
+            if ready.collidepoint(mouse_pos):
+                print(f"Starting night with difficulties - Freddy: {freddif}, Chica: {chicadif}, Bonnie: {bonniedif}, Foxy: {foxydif}")
+                pygame.quit()
+
 
 def CN():
     running = True
